0af7fe60301fb892723f4f79aab15c9c1c64034a
LastComrade0 <badpigg501@gmail.com>
2025-09-11 17:31:28 -0700

    Part 2

--- DataInvestigator.py
+++ DataInvestigator.py
@@ -1,11 +1,28 @@
+import pandas as pd
+
 class DataInvestigator:
+    def __init__(self, df):
+        self.df = df
 
     def baseline(self, col):
-        pass
+        # col is a zero-indexed column number
+        column_data = self.df.iloc[:, col] # iloc[row #, column #]
+        return column_data.mode().iloc[0] # mode() returns the most frequent value
 
     def corr(self, col1, col2):
-        pass
+        correlation = self.df.iloc[:, col1].corr(self.df.iloc[:, col2])
+        return correlation
 
     def zeroR(self, col):
-        pass
+         # col is a zero-indexed column number
+        column_data = self.df.iloc[:, col] # iloc[row #, column #]
+        return column_data.mode().iloc[0] # mode() returns the most frequent value
+
+
+df = pd.read_csv('gallstone.csv')
+di = DataInvestigator(df)
+print(di.baseline(1))
+print(di.corr(1, 2))
+print(di.zeroR(1))
+
 
